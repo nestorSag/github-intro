cd37b7fe4b00a047e1218e17a788879b8ad5b1b2
Magdalena Navarro <magdalena.nta@gmail.com>
2025-10-01 11:44:17 +0100

    added people to hello, created goodbye

---
+++ goodbye.py
@@ -0,0 +1,4 @@
+names = ['Tom']
+
+if __name__ == "__main__":
+    print(f"Adios {', '.join(names)}!")
--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,5 @@
 
-names = ['Nestor', 'Tom']
+names = ['Nestor', 'Tom', 'Magda', 'Ana', 'Luis']
 
 if __name__ == "__main__":
     print(f"Hola {', '.join(names)}!")
